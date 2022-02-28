ad59aadb9c6d99f59ff5c03447ea7827f94f350d
Ryan Hill <ryanhill1128@gmail.com>
2022-02-27 19:44:18 -0600

    iniitalized spring project

---
+++ rook_community_api/src/main/java/io/rookware/rook_community_api/RookCommunityApiApplication.java
@@ -0,0 +1,13 @@
+package io.rookware.rook_community_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookCommunityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RookCommunityApiApplication.class, args);
+	}
+
+}
---
+++ rook_community_api/src/test/java/io/rookware/rook_community_api/RookCommunityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rookware.rook_community_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookCommunityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
